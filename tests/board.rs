822f2d2323ba2eade47826258b0c4c5a4962edaf
Marcos Bianchi <marcobianchi07@gmail.com>
2023-09-07 14:50:17 -0300

    board tests

--- tests/board.rs
+++ tests/board.rs
@@ -1,15 +1,50 @@
 use bomberman::game::board::Board;
+use std::fs::read_to_string;
 
 #[test]
 fn invalid_path() {
-    assert!(Board::new("invalid_path.txt", "").is_err());
-    assert!(Board::new("", "").is_err())
+    assert!(Board::new("invalid_path.txt").is_err());
+    assert!(Board::new("").is_err())
 }
 
-// #[test]
-// fn invalid_output_path() {
-//     let input = "tests/boards/1.txt";
-//     let output = "invalid_output";
-//     let board = Board::new(input, output).unwrap();
-//     assert!(board.save().is_err());
-// }
+#[test]
+fn read_from_file() {
+    let board = Board::new("tests/boards/1.txt").unwrap();
+    let s = read_to_string("tests/boards/1.txt").unwrap();
+    assert_eq!(s, board.to_string());
+}
+
+#[test]
+fn save() {
+    let board = Board::new("tests/boards/savetest.txt").unwrap();
+    board.save("tests/output/savetest.txt").unwrap();
+    let s = read_to_string("tests/output/savetest.txt").unwrap();
+    assert_eq!(board.to_string(), s);
+}
+
+#[test]
+fn complete_example_1() -> Result<(), ()> {
+    let mut board = Board::new("tests/boards/1.txt").unwrap();
+    let s = read_to_string("tests/boards/1R.txt").unwrap();
+    board.pop((0, 0));
+    assert_eq!(s, board.to_string());
+    Ok(())
+}
+
+#[test]
+fn complete_example_2() -> Result<(), ()> {
+    let mut board = Board::new("tests/boards/2.txt").unwrap();
+    let s = read_to_string("tests/boards/2R.txt").unwrap();
+    board.pop((2, 4));
+    assert_eq!(s, board.to_string());
+    Ok(())
+}
+
+#[test]
+fn complete_example_3() -> Result<(), ()> {
+    let mut board = Board::new("tests/boards/3.txt").unwrap();
+    let s = read_to_string("tests/boards/3R.txt").unwrap();
+    board.pop((0, 4));
+    assert_eq!(s, board.to_string());
+    Ok(())
+}
